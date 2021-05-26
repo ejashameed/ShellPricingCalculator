a983b7c0a369c15403913a123b02efdd8a26a9ec
ejashameed <ejashameed@gmail.com>
2021-05-26 20:18:07 +0400

    first commit

---
+++ PricingCalculator/PricingCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PricingCalculator
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
